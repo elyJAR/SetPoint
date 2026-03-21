69242f8f31c8be5fa1f22e154ba61a028e0c5623
ElyJah <elijaholorunfemi@gmail.com>
2026-03-21 04:23:07 +0100

    style: fix status bar visibility on mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.elyjah.setpoint',
   appName: 'SetPoint',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    StatusBar: {
+      backgroundColor: '#f4f5f7',
+      style: 'LIGHT' as any
+    }
+  }
 };
 
 export default config;
